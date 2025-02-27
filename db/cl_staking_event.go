54ee00ef4074feab0e6f2c3b5ffa3a74111a416a
Zerui Ge <gezerui1997@gmail.com>
2025-02-27 16:58:19 -0700

    fix(db): remove unique constriant for index (#21)

--- db/cl_staking_event.go
+++ db/cl_staking_event.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type CLStakingEvent struct {
 	ID          uint64 `gorm:"primarykey"`
-	ELTxHash    string `gorm:"not null;column:el_tx_hash;index:idx_cl_staking_event_el_tx_hash_event_type,priority:1,unique"`
-	EventType   string `gorm:"not null;column:event_type;index:idx_cl_staking_event_el_tx_hash_event_type,priority:2,unique"`
+	ELTxHash    string `gorm:"not null;column:el_tx_hash;index:idx_cl_staking_event_el_tx_hash_event_type,priority:1"`
+	EventType   string `gorm:"not null;column:event_type;index:idx_cl_staking_event_el_tx_hash_event_type,priority:2"`
 	BlockHeight int64  `gorm:"not null;column:block_height;index:idx_cl_staking_event_block_height"`
 	StatusOK    bool   `gorm:"not null;column:status_ok"`
 	ErrorCode   string `gorm:"not null;column:error_code"`
